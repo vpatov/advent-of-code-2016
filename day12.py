eef5e76dfa370a5cabb6b97d8b0a646d252d4e4f
vpatov <vasiapatov@gmail.com>
2017-06-24 11:20:17 -0400

    part 1 of day12

---
+++ day12.py
@@ -0,0 +1,63 @@
+instructions = [line.strip() for line in open('day12.txt','r').readlines()]
+
+registers = {'a':0,'b':0,'c':0,'d':0}
+pc = 0
+
+
+# cpy x y copies x (either an integer or the value of a register) into register y.
+# inc x increases the value of register x by one.
+# dec x decreases the value of register x by one.
+# jnz x y jumps to an instruction y away 
+    # (positive means forward; negative means backward), but only if x is not zero.
+
+def cpy(x,y):
+    global registers
+    if x in registers.keys():
+        registers[y] = registers[x]
+    else:
+        registers[y] = int(x)
+def inc(x,_):
+    global registers
+    registers[x] += 1
+def dec(x,_):
+    global registers
+    registers[x] -= 1
+def jnz(x,y):
+    global pc
+    if x in registers.keys():
+        if (registers[x]):
+            pc += int(y)
+        else:
+            pc += 1
+    else:
+        if int(x):
+            pc += int(y)
+        else:
+            pc += 1
+
+
+operations = {
+    'cpy': cpy,
+    'inc': inc,
+    'dec': dec,
+    'jnz': jnz
+}
+
+# jnz 1 5
+def process_instruction(instruction):
+    global registers, pc
+    parts = instruction.split()
+    opcode = parts[0]
+    op1 = parts[1]
+    op2 = None
+    if (opcode == 'cpy' or opcode == 'jnz'):
+        op2 = parts[2]
+    operations[opcode](op1,op2)
+    if opcode != 'jnz':
+        pc += 1
+
+while (pc < len(instructions)):
+    process_instruction(instructions[pc])
+
+print 'Part I'
+print registers['a']
